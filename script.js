fa8f8254385b95a0bd604e612fc09ffe21932539
Thiago Sanches <115747016+sanchessky@users.noreply.github.com>
2025-10-29 15:14:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
  */
 
-// Banner carrosel ------------
+
 const main = document.querySelector('main')
 const backgrounds = ['url(img/pcgamer1.png)','url(img/pcgamer2.png)','url(img/pcgamer3.png)' ]
 
@@ -14,20 +14,20 @@ function slider(){
     if (indice >= backgrounds.length) {
         indice = 0
     }
-    //troca da imagem de background no CSS
+    
 main.style.backgroundImage = backgrounds[indice]
 main.style.transition = 'background-image 1s ease-in-out'
 }
 
 setInterval(slider, 3000)
 
-// fim do Banner carrosel --------------------------------------------------------------
 
 
-// Inserindo o ano na tag <span> no HTML
+
+
 let ano = document.getElementById('copyrightYear')
 
-// A linha abaixo obtem o ano atual do sistema
+
 let anoAtual = new Date().getFullYear()
-// A linha abaixo atribui o ano a tag <span>
+
 ano.innerHTML = anoAtual
